590827081e7d4232cc7f7cc537d1b7b6fdf510a6
Musa Yazlık <musayazlik1997@gmail.com>
2025-02-27 14:03:11 +0300

    implement NextAuth authentication with Prisma adapter and JWT session strategy

--- auth.ts
+++ auth.ts
@@ -1,5 +1,76 @@
+import Credentials from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/db";
+import bcrypt, { compare } from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth/login",
+    signOut: "/auth/logout",
+    error: "/auth/error",
+  },
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials || !credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const email = credentials.email as string;
+        const hashPassword = bcrypt.hashSync(
+          credentials.password as string,
+          10
+        );
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: email as string,
+          },
+        });
+        if (!user || !user?.password) {
+          return null;
+        }
+        const isCorrectPassword = await compare(
+          hashPassword,
+
+          user.password
+        );
+        if (!isCorrectPassword) {
+          return null;
+        }
+        return {
+          id: user.id,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
+  },
 });
