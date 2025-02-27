a8f693e77fb22ac916d3f1f70a7f44d3d40e1960
Musa Yazlık <musayazlik1997@gmail.com>
2025-02-27 11:19:08 +0300

    implement middleware for session-based redirection in Next.js

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,30 @@
-export { auth as middleware } from "@/auth";
+import { type NextRequest, NextResponse } from "next/server";
+import { auth } from "@/auth";
+
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    if (session) {
+      return NextResponse.redirect(new URL("/panel", request.url));
+    }
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+  if (pathname.startsWith("/panel") && !session) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+  if (pathname.startsWith("/auth") && session) {
+    return NextResponse.redirect(new URL("/panel", request.url));
+  }
+  return NextResponse.next();
+}
 
 export const config = {
-	matcher: "/",
+  matcher: [
+
+    '/',
+    '/panel/:path*',
+    '/auth/:path*',
+  ],
 };
